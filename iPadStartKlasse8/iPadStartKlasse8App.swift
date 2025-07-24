e26cded0b9006e5bb00c713422d01b51b1361ab6
stemega <stefan.megerle@gmail.com>
2025-07-24 17:22:15 +0200

    Initial Commit

---
+++ iPadStartKlasse8/iPadStartKlasse8App.swift
@@ -0,0 +1,17 @@
+//
+//  iPadStartKlasse8App.swift
+//  iPadStartKlasse8
+//
+//  Created by Stefan Megerle on 24.07.25.
+//
+
+import SwiftUI
+
+@main
+struct iPadStartKlasse8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iPadStartKlasse8Tests/iPadStartKlasse8Tests.swift
@@ -0,0 +1,17 @@
+//
+//  iPadStartKlasse8Tests.swift
+//  iPadStartKlasse8Tests
+//
+//  Created by Stefan Megerle on 24.07.25.
+//
+
+import Testing
+@testable import iPadStartKlasse8
+
+struct iPadStartKlasse8Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
