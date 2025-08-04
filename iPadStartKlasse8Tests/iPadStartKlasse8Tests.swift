863469ab1fcde8b599aea2d17c670674bf79d5f7
Ste <116756427+stemega@users.noreply.github.com>
2025-08-04 18:57:17 +0200

    Delete iPadStartKlasse8Tests directory

--- iPadStartKlasse8Tests/iPadStartKlasse8Tests.swift
+++
@@ -1,9 +0,0 @@
-import Testing
-@testable import iPadStartKlasse8
-
-struct iPadStartKlasse8Tests {
-    @Test func faqLoading() throws {
-        let items = FAQStore.loadFAQItems()
-        #expect(!items.isEmpty)
-    }
-}
