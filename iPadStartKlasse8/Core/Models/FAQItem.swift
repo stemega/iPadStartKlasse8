1cf2a13405a44c0ceb0273e14d9bd6879707e6f1
Ste <116756427+stemega@users.noreply.github.com>
2025-08-04 18:56:59 +0200

    Delete iPadStartKlasse8 directory

--- iPadStartKlasse8/Core/Models/FAQItem.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct FAQItem: Identifiable, Codable {
-    var id: String
-    var question: String
-    var answer: String
-    var category: String
-}
--- iPadStartKlasse8/Shared/Constants.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-/// Shared configuration values used across targets.
-enum Constants {
-    /// The App Group identifier used for sharing files between the main app and extensions.
-    static let appGroupID = "group.de.example.iPadStartKlasse8"
-}
